d3564a3daf5e9a0f2a4b822aba32f65b2239bac9
eduardsdn <eduard.sdn@gmail.com>
2023-10-09 11:41:28 +0200

    Home products section layed out

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
 import HomeCSS from "../styles/home.module.css";
+import ButtonsCSS from "../styles/buttons.module.css";
 import imageHero from "../assets/home/desktop/image-hero.jpg";
+import imageSpeakerZX9 from "../assets/home/desktop/image-speaker-zx9.png";
+import imageSpeakerZX7 from "../assets/home/desktop/image-speaker-zx7.jpg";
+import imageEarphonesYX1 from "../assets/home/desktop/image-earphones-yx1.jpg";
+import patterCircles from "../assets/home/desktop/pattern-circles.svg";
 import Categories from "../components/Categories";
 import CompanyDescription from "../components/CompanyDescription";
 import Footer from "../components/Footer";
@@ -16,13 +21,76 @@ export default function Home() {
               Experience natural, lifelike audio and exceptional build quality
               made for the passionate music enthusiast.
             </p>
-            <button className={HomeCSS.seeProductButton}>SEE PRODUCT</button>
+            <button
+              className={`${ButtonsCSS.seeProductBtn} ${HomeCSS.homeHeroBtn}`}
+              id={ButtonsCSS.orange}
+            >
+              see product
+            </button>
           </div>
           <img src={imageHero} className={HomeCSS.imgHero} alt="" />
         </div>
       </div>
       <div className={HomeCSS.container}>
         <Categories></Categories>
+        <div className={HomeCSS.homeProducts}>
+          <div className={HomeCSS.homeProductsTop}>
+            <img
+              className={HomeCSS.homeProductsTopImg}
+              src={imageSpeakerZX9}
+              alt=""
+            />
+            <img
+              className={HomeCSS.homeProductsTopDecor}
+              src={patterCircles}
+              alt=""
+            />
+            <div className={HomeCSS.homeProductsTopContent}>
+              <h1 className={HomeCSS.homeProductsContentTopTitle}>
+                ZX9 SPEAKER
+              </h1>
+              <p className={HomeCSS.homeProductsContentTopDescription}>
+                Upgrade to premium speakers that are phenomenally built to
+                deliver truly remarkable sound.
+              </p>
+              <button
+                className={`${ButtonsCSS.seeProductBtn} ${HomeCSS.homeProductsTopContentBtn}`}
+                id={ButtonsCSS.black}
+              >
+                See product
+              </button>
+            </div>
+          </div>
+          <div className={HomeCSS.homeProductsMiddle}>
+            <div className={HomeCSS.homeProductsMiddleContent}>
+              <h1 className={HomeCSS.homeProductsContentTitle}>ZX7 SPEAKER</h1>
+              <button
+                className={`${ButtonsCSS.seeProductBtn} ${HomeCSS.homeProductsBtn}`}
+                id={ButtonsCSS.black}
+              >
+                see product
+              </button>
+            </div>
+          </div>
+          <div className={HomeCSS.homeProductsBottom}>
+            <img
+              className={HomeCSS.homeProductsBottomImg}
+              src={imageEarphonesYX1}
+              alt=""
+            />
+            <div className={HomeCSS.homeProductsBottomContent}>
+              <h1 className={HomeCSS.homeProductsContentTitle}>
+                YX1 EARPHONES
+              </h1>
+              <button
+                className={`${ButtonsCSS.seeProductBtn} ${HomeCSS.homeProductsBtn}`}
+                id={ButtonsCSS.black}
+              >
+                see product
+              </button>
+            </div>
+          </div>
+        </div>
         <CompanyDescription></CompanyDescription>
       </div>
       <Footer></Footer>
