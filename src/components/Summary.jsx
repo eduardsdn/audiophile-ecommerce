292e62bf65d1bdc62196ce619eb7ad605ce3c41a
eduardsdn <eduard.sdn@gmail.com>
2023-11-23 11:54:03 +0100

    summary component in checkout is functional

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -5,6 +5,34 @@ import CartProductCard from "./CartProductCard";
 
 export default function Summary() {
   const cart = useSelector((state) => state.cart);
+  const shipping = 50;
+  const total = getTotal();
+  const vat = calculateVat();
+  const grandTotal = calculateGrandTotal();
+
+  function getTotal() {
+    let total = 0; // if there are NO products in the cart set total to 0
+    if (cart.length !== 0) {
+      // if there ARE products in the cart calculate total
+      let productsTotals = cart.map(
+        (product) => product.amount * product.price // get total for every individual product type in the cart
+      );
+      total = productsTotals.reduce(
+        (accumulator, currentValue) => accumulator + currentValue // sum all of individual totals
+      );
+    }
+    return total;
+  }
+
+  function calculateVat() {
+    const vat = Math.round((total / 100) * 20);
+    return vat;
+  }
+
+  function calculateGrandTotal() {
+    const grandTotal = total + vat + shipping;
+    return grandTotal;
+  }
 
   return (
     <div className={SummaryCSS.summary}>
@@ -27,9 +55,21 @@ export default function Summary() {
       </div>
       <div className={SummaryCSS.total}>
         <p className={SummaryCSS.totalText}>TOTAL</p>
-        <p className={SummaryCSS.totalNumber}>$ 5692</p>
+        <p className={SummaryCSS.totalValue}>$ {total}</p>
       </div>
-
+      <div className={SummaryCSS.shipping}>
+        <p className={SummaryCSS.shippingText}>SHIPPING</p>
+        {/* VAT is calculated as 20% of the product total, excluding shipping */}
+        <p className={SummaryCSS.shippingValue}>$ {shipping}</p>
+      </div>
+      <div className={SummaryCSS.vat}>
+        <p className={SummaryCSS.vatText}>VAT (INCLUDED)</p>
+        <p className={SummaryCSS.vatValue}>$ {vat}</p>
+      </div>
+      <div className={SummaryCSS.grandTotal}>
+        <p className={SummaryCSS.grandTotalText}>GRAND TOTAL</p>
+        <p className={SummaryCSS.grandTotalValue}>$ {grandTotal}</p>
+      </div>
       <button
         className={`${ButtonsCSS.seeProductBtn} ${ButtonsCSS.checkoutBtn} ${SummaryCSS.continueBtn}`}
         id={ButtonsCSS.orange}
